b042c0602173dc82e4cef590d607fb4185f198d9
alquantumai <owaisibnmushtaq@gmail.com>
2024-05-08 12:17:06 +0530

    project structure and logging, exception created

---
+++ project_workflow.md
@@ -0,0 +1,2 @@
+First I have created project structure and all the necessary directories and files
+setuptools, logging and exception modules created
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="Generalized-AI-Assistant",
+    version="0.0.1",
+    author="Owais Bin Mushtaq",
+    author_email="owais.cqai@gmail.com",
+    package=find_packages(),
+    install_requires=[],
+)
